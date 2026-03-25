0b673ff6dffa2e01c1d38cc6fa9cbb8e87768412
axolhex <axolhex@gmail.com>
2026-03-25 17:49:13 +0000

    Update visorpop.py to v1.0.2

--- src/visorpop.py
+++ src/visorpop.py
@@ -23,14 +23,13 @@ import random
 import logging
 import requests
 import multiprocessing
-import tkinter as tk
 from PIL import Image
 from psutil import pid_exists
 from pymediainfo import MediaInfo
+from screeninfo import get_monitors
 from file_utils import setup_logging, find_app_path, read_settings_file, write_setting
+from tk_utils import get_screen_info
 from popup_player import PopupPlayer
-
-MAIN_SLEEP_TIME: int = 10
 
 def main(parent_pid: int | None = None) -> None:
     #vvv DO NOT REMOVE vvv
@@ -41,7 +40,6 @@ def main(parent_pid: int | None = None) -> None:
     app_path: str
     settings_path: str
     app_path, settings_path = find_app_path()
-    monitor_size: list[int] = determine_screen_size()
     loop_time: int
 
     while True:
@@ -50,28 +48,19 @@ def main(parent_pid: int | None = None) -> None:
                 sys.exit()
         except TypeError:
             pass
-        popup_link, loop_time = open_popup(popup_link, app_path, settings_path, monitor_size)
+        popup_link, loop_time = open_popup(popup_link, app_path, settings_path)
         time.sleep(loop_time)
 
-def determine_screen_size() -> list[int]:
-    root = tk.Tk()
-    root.withdraw()
-
-    width: int = root.winfo_screenwidth()
-    height: int = root.winfo_screenheight()
-    root.destroy()
-    return [width, height]
-
 #Checks for new pop-ups and opens them. Returns pop-up's url and sleep time
-def open_popup(popup_url: str, app_folder: str, settings_file: str, screen_size: list[int]) -> tuple[str, int]:
+def open_popup(popup_url: str, app_folder: str, settings_file: str) -> tuple[str, int]:
     settings: dict | None = read_settings_file(settings_file)
     if settings is None:
-        return popup_url, MAIN_SLEEP_TIME
+        return popup_url, 10
     link_info: dict | None
-    sleep_time: int
-    link_info, sleep_time = get_json_data(f"https://walltaker.joi.how/api/links/{settings["link_id"]}.json")
+    delay_time: int
+    link_info, delay_time = get_json_data(f"https://walltaker.joi.how/api/links/{settings["link_id"]}.json", settings["poll_delay"])
     if link_info is None or link_info["post_url"] is None:
-        return popup_url, sleep_time
+        return popup_url, delay_time
 
     if link_info["post_url"] != popup_url:
         try:
@@ -85,20 +74,32 @@ def open_popup(popup_url: str, app_folder: str, settings_file: str, screen_size:
         size: list[int] | None
         input_conf: str | None
         download_data: requests.models.Response | None
-        sleep_time: int
-        duration, size, input_conf, download_data, sleep_time = get_media_info(app_folder, link_info["post_url"], file_type)
+        delay_time: int
+        duration, size, input_conf, download_data, delay_time = get_media_info(link_info["post_url"], file_type, app_folder, settings["poll_delay"])
         if None in [duration, size, input_conf, download_data]:
-            return popup_url, sleep_time
+            return popup_url, delay_time
 
         #Change duration based on time limit
         loop_file: str = 'inf'
-        if settings["auto_close"]:
+        if settings["time_limit"]:
             #Disable auto close if play video is enabled. Have mpv close window at end of file instead
             if settings["play_video"] and duration > settings["time_limit"]:
-                settings["auto_close"] = False
+                settings["time_limit"] = False
                 loop_file: bool = False
             else:
                 duration = settings["time_limit"]
+
+        screens: list = []
+        for index, monitor in enumerate(settings["monitors"]):
+            if monitor:
+                screens.append(index)
+        screen_size: list[int]
+        screen_position: list[int]
+        try:
+            screen_size, screen_position = get_screen_info(get_monitors()[random.sample(screens, 1)[0]])
+        except Exception as err:
+            logging.error(repr(err))
+            return popup_url, settings["poll_delay"]
 
         if not settings["fullscreen"]:
             #Reduce window size if exceeding maximum size
@@ -112,10 +113,10 @@ def open_popup(popup_url: str, app_folder: str, settings_file: str, screen_size:
             size = screen_size
 
         #Set random window position
-        position: list[int] = [0, 0]
+        position: list[int] = screen_position
         for i in 0, 1:
             try:
-                position[i] = random.randrange(0, ((screen_size[i]) - size[i]) + 1)
+                position[i] = random.randrange(screen_position[i], (((screen_size[i]) - size[i]) + screen_position[i]) + 1)
             except ValueError:
                 pass
         logging.info(f"Duration: {duration} | Size: {size[0]}, {size[1]} | Position: {position[0]}, {position[1]}")
@@ -124,12 +125,12 @@ def open_popup(popup_url: str, app_folder: str, settings_file: str, screen_size:
         if settings["download"]:
             file_path = download_popup(settings["save_path"], file_name, download_data)
             if file_path is None:
-                return popup_url, MAIN_SLEEP_TIME
+                return popup_url, settings["poll_delay"]
 
         #Start pop-up
         multiprocessing.Process(target=PopupPlayer,
                                 args=(
-                                    settings["auto_close"],
+                                    settings["time_limit"],
                                     settings["notif_volume"],
                                     settings["video_volume"],
                                     file_name,
@@ -146,24 +147,24 @@ def open_popup(popup_url: str, app_folder: str, settings_file: str, screen_size:
                                 ).start()
         write_setting(settings_file, 'Settings', 'popup_count', settings["popup_count"] + 1)
         popup_url = link_info["post_url"]
-    return popup_url, MAIN_SLEEP_TIME
+    return popup_url, settings["poll_delay"]
 
 #Returns user data
-def get_json_data(json_url: str) -> tuple[dict, int] | tuple[None, int]:
+def get_json_data(json_url: str, poll_delay: int) -> tuple[dict, int] | tuple[None, int]:
     json_data: requests.models.Response | None
-    wait_time: int
-    json_data, wait_time = read_url(json_url, MAIN_SLEEP_TIME)
+    sleep_time: int
+    json_data, sleep_time = read_url(json_url, poll_delay)
     if json_data is None:
-        return None, wait_time
-    return json_data.json(), MAIN_SLEEP_TIME
+        return None, sleep_time
+    return json_data.json(), poll_delay
 
 #Returns duration, size, mpv conf and download data
-def get_media_info(path: str, url: str, extension: str) -> tuple[float, list[int], str, requests.models.Response, int] | tuple[None, None, None, None, int]:
+def get_media_info(url: str, extension: str, path: str, poll_delay: int) -> tuple[float, list[int], str, requests.models.Response, int] | tuple[None, None, None, None, int]:
     media: requests.models.Response | None
-    wait_time: int
-    media, wait_time = read_url(url, 30)
+    sleep_time: int
+    media, sleep_time = read_url(url, poll_delay * 2)
     if media is None:
-        return None, None, None, None, wait_time
+        return None, None, None, None, sleep_time // 2
 
     if extension == "webm" or extension == "mp4":
         video = MediaInfo.parse(io.BytesIO(media.content))
@@ -183,7 +184,7 @@ def get_media_info(path: str, url: str, extension: str) -> tuple[float, list[int
                     break
             resolution: list[int] = [image.size[0], image.size[1]]
             mpv_input: str = f'{path}/data/input_image.conf'
-    return milliseconds / 1000, resolution, mpv_input, media, MAIN_SLEEP_TIME
+    return milliseconds / 1000, resolution, mpv_input, media, poll_delay
 
 #Downloads pop-up and returns file path
 def download_popup(save_path: str, save_name: str, save_data: requests.models.Response) -> str | None:
@@ -205,8 +206,8 @@ def read_url(link:str, timeout_time: int) -> tuple[requests.models.Response, int
         return None, 0
     except Exception as err:
         logging.error(repr(err))
-        return None, MAIN_SLEEP_TIME
-    return response, MAIN_SLEEP_TIME
+        return None, timeout_time
+    return response, timeout_time
 
 if __name__ == "__main__":
     multiprocessing.set_start_method('spawn')
